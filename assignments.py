8b40205c72de62e174f9855f2f201f8c5e99633f
alamtariq143 <alamtariq143@gmail.com>
2025-06-18 14:52:17 +0530

    initial commit

---
+++ assignments.py
@@ -0,0 +1,19 @@
+# Task 1: Perform Basic Mathematical Operations
+inputA = int(input("Enter the first number: "))
+inputB = int(input("Enter the second number: "))
+
+add = inputA+inputB
+print('Addition :', add)
+sub = inputA-inputB
+print('Subtraction :', sub)
+mul = inputA*inputB
+print('Multiplication :', mul)
+div = inputA/inputB
+print('Division :', div)
+
+# Task 2: Create a Personalized Greeting
+
+inputC = input("Enter yoyr first name: ")
+inputD = input("Enter yoyr last name: ")
+concat = inputC + inputD
+print('Hello,'+' '+inputC +' '+ inputD+'!', 'Welcome to the Python program.')
